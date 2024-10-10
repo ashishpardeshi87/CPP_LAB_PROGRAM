7cfcce254f4c45fbe08117203c2c2bad5ea2eace
Ashish Pardeshi <143716256+ashishpardeshi87@users.noreply.github.com>
2024-10-10 09:40:09 +0530

    Add files via upload
    This C++ program demonstrates the use of logical operators. It performs the following operations on two integers `a = 10` and `b = 4`:
    
    1. **Logical AND (`&&`)**
    2. **Logical OR (`||`)**
    3. **Logical NOT (`!`)**
    
    The program displays the results of these logical operations.

---
+++ lab pro12 logical operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int a=10 , b=4;
+  
+  // Logical AND operator
+  cout<<"a&&b is"<<(a&&b)<<endl;
+  
+  //Logical OR operator
+  cout<<"a||b is "<<(a||b)<<endl;
+  
+  //Logical NOT operator 
+  cout<<"!b is "<<(!a)<<endl;
+  
+  return 0;
+  
+}
+
+
+Output:
+
+a&&b is1
+a||b is 1
+!b is 0
